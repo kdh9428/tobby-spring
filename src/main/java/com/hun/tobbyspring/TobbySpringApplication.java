74fb068cdb6adce27fa8a27b8d1486abd4c3b462
dahun <kim23222@naver.com>
2022-03-07 13:34:00 +0900

    init application
    프로젝트 생성

---
+++ src/main/java/com/hun/tobbyspring/TobbySpringApplication.java
@@ -0,0 +1,13 @@
+package com.hun.tobbyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobbySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobbySpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hun/tobbyspring/TobbySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hun.tobbyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobbySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
