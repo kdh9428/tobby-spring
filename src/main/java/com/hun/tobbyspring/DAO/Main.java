54cc61d0132c7dc1de26df91dd6984a50a00b7ce
dahun <kim23222@naver.com>
2022-03-12 23:17:25 +0900

    [추가] main 메서드로 테스트 코드 작성, 유저 엔티티, db 커넥터

---
+++ src/main/java/com/hun/tobbyspring/DAO/Main.java
@@ -0,0 +1,29 @@
+package com.hun.tobbyspring.DAO;
+
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        UserDao dao = new NUserDao();
+
+        User user = new User();
+        user.setId("kdh");
+        user.setName("김다훈");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + "등록 성공");
+
+        User user2 = dao.get(user.getId());
+
+        System.out.println(user2.getName());
+
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + "조회 성공");
+
+    }
+}
